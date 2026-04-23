1f204424fab8f6cd622043068b4735d621202546
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-04-22 23:47:46 -0300

    feat: add ICategoryRepository interface
    - Created ICategoryRepository to define category data access contract
    - Added CRUD operations for Category entity
    - Implemented paginated retrieval for categories
    - Added method to fetch category by name

--- src/TradeStore.Domain/obj/Debug/net8.0/TradeStore.Domain.AssemblyInfo.cs
+++ src/TradeStore.Domain/obj/Debug/net8.0/TradeStore.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradeStore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f0f52073030cd44921381aff46584cb8cc2436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08c3b7e227007a9237a0d6cc78749b05054da35")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradeStore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradeStore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
