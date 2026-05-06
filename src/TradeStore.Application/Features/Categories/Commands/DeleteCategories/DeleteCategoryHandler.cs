20924879228056f947c3260d1f145e93866cfe16
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-05-05 22:46:05 -0300

    feat(category): add DeleteCategoryCommand and DeleteCategoryHandler
    - Implement DeleteCategoryCommand
    - Implement DeleteCategoryHandler
    - Add logic for category deletion

---
+++ src/TradeStore.Application/Features/Categories/Commands/DeleteCategories/DeleteCategoryHandler.cs
@@ -0,0 +1,25 @@
+using MediatR;
+using TradeStore.Domain.Repositories;
+
+namespace TradeStore.Application.Features.Categories.Commands.DeleteCategories;
+
+public class DeleteCategoryHandler : IRequestHandler<DeleteCategoryCommand, bool>
+{
+    private readonly ICategoryRepository _repository;
+
+    public DeleteCategoryHandler(ICategoryRepository respository)
+    {
+        _repository = respository;
+    }
+
+    public async Task<bool> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await _repository.GetByIdAsync(request.Id);
+
+        if(category is null) return false;
+
+        await _repository.DeleteAsync(request.Id);
+
+        return true;
+    }
+}
--- src/TradeStore.Application/obj/Debug/net8.0/TradeStore.Application.AssemblyInfo.cs
+++ src/TradeStore.Application/obj/Debug/net8.0/TradeStore.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradeStore.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9742708a8cfb5d62d58c0ef1c5e91a761ec2eb7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9e736d55996d319507114ab090a1d9ee87a87d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradeStore.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradeStore.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
