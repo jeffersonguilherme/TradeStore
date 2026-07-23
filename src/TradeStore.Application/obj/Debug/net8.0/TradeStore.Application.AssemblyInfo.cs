ad996204b8192aebfb18da1fae1c364d661d3a4f
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-07-22 22:03:18 -0300

    feat: add GetProductTypeByName query and handler
    - Added the GetProductTypeByName query.
    - Implemented the corresponding handler.
    - Enabled product type retrieval by name.

--- src/TradeStore.Application/obj/Debug/net8.0/TradeStore.Application.AssemblyInfo.cs
+++ src/TradeStore.Application/obj/Debug/net8.0/TradeStore.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradeStore.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7b40f6c1649c2264a0ed5e3592cda73ba801af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef5671adfbe884a9fb55212f138d275a8e20895")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradeStore.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradeStore.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
